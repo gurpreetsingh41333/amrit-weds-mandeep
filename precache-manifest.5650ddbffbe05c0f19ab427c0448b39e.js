e82583535e7368ed5c7419af82e704d0ae750490
Gurpreet Singh <gurpreet.singh1@agreeya.net>
2019-10-05 23:46:12 +0530

    Updates

---
+++ precache-manifest.5650ddbffbe05c0f19ab427c0448b39e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7d0ce91129b93cabfcdaf9667cc649",
+    "url": "/amrit-weds-mandeep/index.html"
+  },
+  {
+    "revision": "55a577cd4aa421ecbfdf",
+    "url": "/amrit-weds-mandeep/static/js/2.def50c6c.chunk.js"
+  },
+  {
+    "revision": "0c283f6e33c8cae7fcdb",
+    "url": "/amrit-weds-mandeep/static/js/main.0b1c840a.chunk.js"
+  },
+  {
+    "revision": "a206665a616d66da18d8",
+    "url": "/amrit-weds-mandeep/static/js/runtime-main.83f79547.js"
+  }
+]);
